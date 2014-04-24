bad519060f5ca7e4019f02c0b3d4a8ae0e788c12
Luyue <lzhao49@wisc.edu>
2014-04-23 23:33:24 -0500

    Complete chapter11

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -30,7 +30,17 @@ describe "Static pages" do
             expect(page).to have_selector("li##{item.id}", text: item.content)
           end
         end
+        describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
       end
+    end
 
       describe "should have correct micropost counts at the sidebar" do
        context "for a singular post" do
@@ -53,7 +63,6 @@ describe "Static pages" do
       end
     end
   end
-
   describe "Help page" do
     before { visit help_path }
 
